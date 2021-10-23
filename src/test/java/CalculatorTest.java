971feb19586d0d5c9e6c59d0361718a7770b31c1
florianhoelbling <s50721@edu.campus02.at>
2021-10-23 12:16:17 +0200

    adding test environment

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void subtract() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void divide() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void modulo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sum() {
+    }
+}
