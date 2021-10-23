4e81e773edfa3e393664267405393c3a14caa803
florianhoelbling <s50721@edu.campus02.at>
2021-10-23 12:21:50 +0200

    adding add test methode

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,9 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
     @org.junit.jupiter.api.Test
     void add() {
+        Calculator calculator = new Calculator();
+        int a = 1;
+        int b = 2;
+        final double result = calculator.add(a, b);
+        Assertions.assertEquals(3, result);
     }
 
     @org.junit.jupiter.api.Test
